49c4c5337399ee6a182b533175d76d0f24bbc4d4
ohuton55 <tttt.tttt.no@gmail.com>
2023-01-17 20:57:55 +0900

    add WORKING-STORAGE SECTION

--- test1.cbl
+++ test1.cbl
@@ -1,3 +1,4 @@
+
       ******************************************************************
       * Author:
       * Date:
@@ -9,11 +10,20 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01 HELLO1       PIC X(15).
+       01 HELLO2.
+           03 FILLER PIC X(06) VALUE 'HELLO,'.
+           03 FILLER PIC X(1)  VALUE SPACE.
+           03 FILLER PIC X(5)  VALUE 'WORLD'.
+           03 HINITI.
+               05 FILLER   PIC 9(4) VALUE 2023.
+               05 FILLER   PIC 9(2) VALUE 01.
+               05 FILLER   PIC 9(2) VALUE 17.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
             DISPLAY "Hey Girls!"
+            DISPLAY HINITI.
             STOP RUN.
        END PROGRAM SAYHEY.
-
 
