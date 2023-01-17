842ee9b4f7d426318b969e3bb1721a78e357334f
ohuton55 <tttt.tttt.no@gmail.com>
2023-01-17 20:49:56 +0900

    try OpenCobolIDE

---
+++ test1.cbl
@@ -0,0 +1,19 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SAYHEY.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            DISPLAY "Hey Girls!"
+            STOP RUN.
+       END PROGRAM SAYHEY.
+
+
